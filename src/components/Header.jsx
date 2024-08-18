14dcdc143b641a54a5d38cbf07168b870375579e
Raninder Dhillon <37101702+raninder@users.noreply.github.com>
2024-08-18 19:48:42 -0400

    header created with menu items

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import MenuItem from './MenuItem';
+import { AiFillHome } from 'react-icons/ai';
+import { BsFillInfoCircleFill } from 'react-icons/bs';
+
+export default function Header() {
+  return (
+    <div>
+        <div className=''>
+            <MenuItem title="home" address="/" Icon={AiFillHome} />
+            <MenuItem title='about' address='/about' Icon={BsFillInfoCircleFill} />
+        </div>
+        <div className=''>
+            
+        </div>
+    </div>
+  )
+}
