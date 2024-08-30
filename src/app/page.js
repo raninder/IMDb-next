e5e4247a70898a47d02b5ccfec7fce01e602c30a
Raninder Dhillon <37101702+raninder@users.noreply.github.com>
2024-08-30 12:45:02 -0400

    add loading effect

--- src/app/page.js
+++ src/app/page.js
@@ -8,8 +8,22 @@ export default async function Home({searchParams}) {
     `https://api.themoviedb.org/3${
       genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
     }?api_key=${API_KEY}&language=en-US&page=1`,
-    // { next: { revalidate: 10000 } }
+    { next: { revalidate: 10000 } }
   );
+
+  //Temporary part for loading effect
+  // const res = await new Promise((resolve) => {
+  //   setTimeout(async()=>{
+  //     const response = await fetch(
+  //   `https://api.themoviedb.org/3${
+  //     genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
+  //   }?api_key=${API_KEY}&language=en-US&page=1`,
+  //   { next: { revalidate: 10000 } }
+  // );
+  // resolve(response);
+  // }, 2000)
+  // });
+
   const data = await res.json();
   if (!res.ok) {
     throw new Error('Failed to fetch data');
