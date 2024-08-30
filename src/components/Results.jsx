f46735fa3a2de5f7a28c078541c291359dc4a771
Raninder Dhillon <37101702+raninder@users.noreply.github.com>
2024-08-29 23:22:12 -0400

    fetch data from API

---
+++ src/components/Results.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Results({results}) {
+  return (
+    <div>
+        Results
+        {results.map((result)=>(
+            <div key={result.id}>
+                <h2> {result.title}</h2>
+            </div>
+        ))}
+    </div>
+  )
+}
